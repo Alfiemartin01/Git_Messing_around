6adc49e2e600c11b6b4d76d7e60037426105a3a9
Alfie Martin <alfiemartin01@gmail.com>
2023-07-17 11:49:37 +0100

    Games Database has new records added

--- Games_SQL.sql
+++ Games_SQL.sql
@@ -68,8 +68,8 @@ Employee_ID INT AUTO_INCREMENT,
 Emp_First_Name VARCHAR(30) NOT NULL,
 Emp_Last_Name VARCHAR(30) NOT NULL,
 SALARY INT NOT NULL,
-Emp_EMAIL VARCHAR(60) NOT NULL,
-Emp_Phone_number INT NOT NULL,
+Emp_Email VARCHAR(60) NOT NULL,
+Emp_Phone_number VARCHAR(11) NOT NULL,
 PRIMARY KEY(Employee_ID)
 );
 
@@ -81,7 +81,42 @@ PRIMARY KEY(Employee_ID)
 CREATE TABLE Supplier(
 Supplier_ID INT AUTO_INCREMENT,
 Supplier_name VARCHAR(50) NOT NULL,
-Sup_Phone_number INT,
+Sup_Phone_number VARCHAR(11),
 Sup_Email VARCHAR(60),
 PRIMARY KEY(Supplier_ID)
 );
+
+INSERT INTO Supplier(Supplier_name,Sup_Phone_number,Sup_Email)
+VALUES
+('Game_Freak','07123456789','game_freak@freak_game.com'),
+('Wiazards_of_the_coast','07123423254','WOTC@WOTC.com');
+
+INSERT INTO Employee(Emp_First_Name.Emp_Last_Name,Salary,Emp_Phone_number,Emp_Email)
+VALUES
+('Athena','Smith',24000,'07384637346','Athena.s@bfgs.com'),
+('Joy','Campbell',29000,'07385769475','Joy.c@bfgs.com');
+
+INSERT INTO Customer(cust_First_Name,cust_Last_Name,cust_Phone_number,cust_Email)
+VALUES
+('Hazel','Frampton','07946372857','HFramp@gmail.com'),
+('Isabella','Stephens','07263549806',"IzzyStephens@yahoo.co.uk");
+
+INSERT INTO Games(Game_genre,Game_Name,Game_Price,Stock,Rating,FK_Supplier_ID)
+VALUES
+('Strategy','Magic_The_Gathering',22.99,19,3.4,2)
+('RPG','Final_Fantasy_XVI',109.01,1,4.8,1);
+
+INSERT INTO Transaction(Date_time,FK_Customer_ID,FK_Employee_ID,FK_Game_ID)
+VALUES
+('2023-07-07',1,2,1),
+('2023-03-17',2,2,1);
+
+SELECT Emp_First_Name,Salary
+FROM Employee
+WHERE Salary > 25000
+UPDATE Employee
+SET Salary = 25000;
+
+DELETE 
+FROM Games
+WHERE Game_Price > 100;
