303cd31ab779a7f1d85bb08b03aba9f42d86cf98
Alfie Martin <alfiemartin01@gmail.com>
2023-07-18 12:11:49 +0100

    Added exercise python lists

---
+++ Lists_Python.py
@@ -0,0 +1,4 @@
+books = {'JKrowling':'Harry Potter','Lewis Carroll':['Alice in Wonderland','I don\'t know what else she wrote']}
+
+u_author = str(input("Please ive the fullname of an author")) 
+print(', '.join(books[u_author]))
