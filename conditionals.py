74993648efc0a821f4f709a8b469e9ff9e0bcbf2
Alfie Martin <alfiemartin01@gmail.com>
2023-07-18 14:11:44 +0100

    Accounted for cheaters

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,9 @@
 mark = int(input("Please input a mark between 0 and 100"))
 
 
-if mark > 85:
+if mark > 100:
+    print("Some cheatery is afoot")
+elif mark > 85:
     print("Distiction")
 elif 65 <= mark <= 85:
     print("Pass")
@@ -13,5 +15,7 @@ if mark > 85:
     print("Distiction")
 if 65 <= mark <= 85:
     print("Pass")
+if mark > 100:
+    print("Some cheatery is afoot")
 if mark < 65:
     print("Fail")
