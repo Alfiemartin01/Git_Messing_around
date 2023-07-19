0141db373a3619f0098acdb01e8a96e303de16a8
Alfie Martin <alfiemartin01@gmail.com>
2023-07-19 16:31:11 +0100

    MORE FUNCTIONS

--- Python/functions.py
+++ Python/functions.py
@@ -12,7 +12,7 @@ def grade(name,hscore,ascore,escore):
 
     return(f"{name} scored {hscore + ascore + escore} our of 175 getting them a {ograde}")
     
-print(grade(input("name"),int(input("Homework Score")),int(input("Assessment Score")),int(input("Exam Score"))))
+#print(grade(input("name"),int(input("Homework Score")),int(input("Assessment Score")),int(input("Exam Score"))))
 
 
 # 1. Write a Python function to find the maximum of three numbers.
@@ -92,6 +92,82 @@ def isprime(num1):
     return("Prime")
 
 print(isprime(97))
+
+#10. Write a Python program to print the even numbers from a given list.
+
+def evenlist(listA):
+    ret_list = []
+    for i in listA:
+        if i%2 == 0:
+            ret_list.append(i)
+    return(ret_list)
+
+print(evenlist([1,2,3,4,5,6]))
+
+#11. Write a Python function to check whether a number is "Perfect" or not.
+
+def factor_num(num1):
+    factors = []
+    for i in range(1,num1):
+        if num1%i == 0:
+            factors.append(i)
+    return factors
+
+def perf_num(num1):
+    if num1 == sum(factor_num(num1)):
+        return True
+    return False
+
+print(perf_num(8128))
+
+#12. Write a Python function that checks whether a passed string is a palindrome or not.
+
+def ispalin(str1):
+    
+    str1 = (str1.replace(" ","")).lower()
+    for i in range(0,len(str1)//2):
+        if str1[i] != str1[-(i+1)]:
+            return False
+    return True
+
+print(ispalin("Race car"))
+
+#13. Write a Python function to check whether a string is a pangram or not.
+
+def pangram(str1):
+    alphaset = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    str1 = str1.lower()
+    for i in alphaset:
+        if not(i in str1):
+            return False
+    return True
+ 
+print(pangram('The quick brown fox jumps over the lazy dog'))         
+
+#14. Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically.
+
+def hyphensep(str1):
+    str1 = str1.lower()
+    listA = str1.split("-")
+    listA.sort()
+    return "-".join(listA)
+
+print(hyphensep("Hello-World-welcome-to-the-madness"))
+
+#15. 16. Write a Python function to create and print a list where the values are the squares of numbers between 1 and 30 (both included).
+
+def sqaures30():
+    squares = []
+    for i in range(1,31):
+        squares.append(i**2)
+    return(squares)
+
+print(sqaures30())
+
+
+
+
+
 
 
 
