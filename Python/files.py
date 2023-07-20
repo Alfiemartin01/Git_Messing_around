5f2c745087b132b43a3a312eeea07498ec7a403c
Alfie Martin <alfiemartin01@gmail.com>
2023-07-20 16:13:08 +0100

    completed some file exercises

--- Python/files.py
+++ Python/files.py
@@ -20,3 +20,62 @@ for i in filelist:
 file.close()
 
 
+#1. Write a Python program to read an entire text file.
+file = open('file1.txt','r')
+
+print(file.readlines())
+file.close()
+
+#2. Write a Python program to read first n lines of a file.
+file = open('teams.txt','r')
+
+for i in range(int(input("How Many Lines?"))):
+    print(file.readline())
+
+file.close()
+
+
+#3. Write a Python program to append text to a file and display the text.
+
+file = open("teams.txt","a")
+
+file.write('Ha lines go brrrr')
+
+file.close()
+
+file = open("teams.txt","r")
+
+print(file.readlines())
+
+file.close()
+
+#4. Write a Python program to read last n lines of a file.
+
+file = open("teams.txt","r")
+
+filelist = file.readlines()
+print(filelist[len(filelist)-int(input("how many lines at the end?")):])
+
+file.close()
+
+#10. Write a Python program to count the frequency of words in a file.
+
+file = open('file1.txt','r')
+
+filelist = file.readlines()
+
+for i in range(len(filelist)):
+    filelist[i] = filelist[i].replace("\n","")
+    filelist[i] = filelist[i].replace(".","")
+    filelist[i] = filelist[i].split(" ")
+
+worddict = {}
+for i in filelist:
+    for j in i:
+        if not(j in worddict):
+            worddict[j] = 0
+        worddict[j] += 1 
+
+for i in worddict:
+    print(f"{i} appears {worddict[i]} times")
+
