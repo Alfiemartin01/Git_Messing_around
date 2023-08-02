b8be414512caa404764714503a4e9481d7dede9a
Alfie Martin <alfiemartin01@gmail.com>
2023-08-02 16:01:56 +0100

    intro to JS completed

---
+++ HTML. CSS. JS/CheatSheets/JSCheatSHEET.js
@@ -0,0 +1,23 @@
+// Console Outputs
+console.log('This is a console log');
+console.info('This is a console info');
+console.warn('This is a console warning');
+console.error('This is a console error');
+
+let Var1 = 100;
+const Const1 = 5;
+
+console.log("this is a %c a message with some CSS."+myTxt,"color: yellow; font-style: italic; background-color: blue;padding: 2px");
+console.log(`This allows us to implement vars in strings like ${Var1}`)
+//Primitive data types - Immutable, fixed length, quick to look up
+////Boolean
+////Number
+////BigInt
+////String
+////Undefined - not ben assigned a value
+////Null - value that can be assigned to rep objects that dont exist
+////Symbol
+//Object - collections of properties, mutable
+
+typeof(Var1); //returns the var type
+
---
+++ HTML. CSS. JS/Exercises/introtojs.js
@@ -0,0 +1,11 @@
+console.log(`Athena`);
+console.info(`Martin`);
+console.warn(`Portsmouth,UK`);
+console.error(`Taurus (I think?)`);
+
+make = 'Audi';
+model = 'A5';
+
+console.log(`My favourite car is ${make} and the model is ${model}`);
+
+console.log(`%c this text will have css properties`,'color:orange; font-family; fantasy; font-style: bold; background-color; black; padding: 10px');
